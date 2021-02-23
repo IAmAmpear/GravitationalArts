a2aa52b77842de6697482abc15dc27d9508e3533
Bernardo Pereira <bernardowebdev@outlook.com>
2021-02-23 16:04:54 +0000

    fix heroku setup

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function (req, res) {
-  console.log("Server started on port 3000.");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
